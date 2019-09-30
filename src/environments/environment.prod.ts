cc17cb9c2696ff493e8196d07e87f4fcb0d887dc
yhatanaka0804 <yhatanaka0804@gmail.com>
2019-09-30 14:34:40 +0900

    Firebase設定

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAB1K-vzpdnkDZCO0qcJq_CJl6x19TUQLQ',
+    authDomain: 'ionic-chat-fec10.firebaseapp.com',
+    databaseURL: 'https://ionic-chat-fec10.firebaseio.com',
+    projectId: 'ionic-chat-fec10',
+    storageBucket: '',
+    messagingSenderId: '154859996819',
+    appId: ' 1:154859996819:web:40874da77c183d51e1ef66'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAB1K-vzpdnkDZCO0qcJq_CJl6x19TUQLQ',
+    authDomain: 'ionic-chat-fec10.firebaseapp.com',
+    databaseURL: 'https://ionic-chat-fec10.firebaseio.com',
+    projectId: 'ionic-chat-fec10',
+    storageBucket: '',
+    messagingSenderId: '154859996819',
+    appId: ' 1:154859996819:web:40874da77c183d51e1ef66'
+  }
 };
 
 /*
